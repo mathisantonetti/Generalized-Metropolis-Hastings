57b5f707a2b96251537d159d2000d83dda221313
Mathis Antonetti <mathis.antonetti@gmail.com>
2024-03-15 00:06:41 +0100

    final commit

--- GMH/MCMC.py
+++ GMH/MCMC.py
@@ -2,7 +2,25 @@ import numpy as np
 
 def Gaussian_distrib(mu, sigma):
     d = mu.shape[0]
-    return lambda x:np.exp(-(x-mu)@np.linalg.solve(sigma, x - mu))/np.sqrt(2*np.pi)**(d/2)
+    return lambda x:np.exp(-(x-mu)@np.linalg.solve(sigma, x - mu)/2.0)
+
+def banana_distrib(mu, sigma, a, b):
+    invbanana = lambda x:np.array([x[0]/a, a*x[1] - a*b*((x[0]/a)**2+a**2)])
+
+    return lambda x:np.exp(-(invbanana(x)-mu)@np.linalg.solve(sigma, invbanana(x) - mu)/2.0)
+
+def banana_sample(a, b, mu, Sigma, N):
+    u = np.random.multivariate_normal(mu, Sigma, size=N)
+    x = a*u[:, 0]
+    y = u[:, 1]/a+b*(u[:, 0]**2+a**2)
+    return x, y
+
+def compare_bananas(X, Y, a, b):
+    Z1 = np.array([X[:, 0]/a, a*X[:, 1] - a*b*((X[:, 0]/a)**2+a**2)]) # First gaussian
+    Z2 = np.array([Y[:, 0]/a, a*Y[:, 1] - a*b*((Y[:, 0]/a)**2+a**2)]) # Second gaussian
+
+    return np.abs(Z1.mean() - Z2.mean()), np.abs(Z1.std() - Z2.std())
+
 
 def ESS(data):
     n, m = data.shape
@@ -35,13 +53,16 @@ class GaussianKernel():
         self.mu = mu
 
     def __call__(self, x, y):
-        return np.prod([np.exp(-(y[i]-self.mu(x))@np.linalg.solve(self.sigma(x), y[i] - self.mu(x)))/np.sqrt(2*np.pi)**(x.shape[0]/2) for i in range(y.shape[0])])
+        return np.prod([np.exp(-(y[i]-self.mu(x))@np.linalg.solve(self.sigma(x), y[i] - self.mu(x))/2.0) for i in range(y.shape[0])])
     
     def sample(self, x, N):
         return np.random.multivariate_normal(self.mu(x), self.sigma(x), size=N)
     
 
-def Generalized_Metropolis_Hastings(distrib, kernel, x0, T, N):
+def Generalized_Metropolis_Hastings(distrib, kernel, x0, T, N, M=None):
+    if(M is None):
+        M = N
+
     d = x0.shape[0]
     xtilde = np.zeros((N+1, d))
     x = []
@@ -65,7 +86,7 @@ def Generalized_Metropolis_Hastings(distrib, kernel, x0, T, N):
         pIcond = pIcond/np.sum(pIcond)
 
         # Step 3
-        for m in range(N):
+        for m in range(M):
             I = np.random.choice(N+1, p=pIcond)
             x.append(np.copy(xtilde[I]))
 
@@ -88,18 +109,20 @@ def Truly_Generalized_Metropolis_Hastings(distrib, kernel, x0, T, N):
         xtilde[I+1:, :] = new_xtilde[I:, :]
         
         # Step 2
-        pIcond = np.zeros(N+1)
-        piK_I = distrib(xtilde[I])*kernel(xtilde[I], np.concatenate((xtilde[:I], xtilde[I+1:]), axis=0))
-        for j in range(N+1):
-            if(j != I):
-                pIcond[j] = min(1, distrib(xtilde[j])*kernel(xtilde[j], np.concatenate((xtilde[:j], xtilde[j+1:]), axis=0))/piK_I)/N
-                pIcond[I] += pIcond[j]
-
-        pIcond[I] = 1 - pIcond[I]
+        concat = []
+        for i in range(N+1):
+            concat.append(np.concatenate((xtilde[1:i], xtilde[i+1:]), axis=0))
+        
+        A = np.zeros((N+1, N+1))
+        for i in range(N+1):
+            for j in range(N+1):
+                if(j != i):
+                    A[i,j] = min(1, distrib(xtilde[i])*kernel(xtilde[i], concat[i])/distrib(xtilde[j])*kernel(xtilde[j], concat[j]))/N
+            A[i,i] = 1.0 - np.sum(A[i])
 
         # Step 3
         for m in range(N):
-            I = np.random.choice(N+1, p=pIcond)
+            I = np.random.choice(N+1, p=A[I])
             x.append(np.copy(xtilde[I]))
 
     return x
--- README.md
+++ README.md
@@ -3,6 +3,17 @@
 This code is a Python implementation of the algorithm described in the paper : 
 B. Calderhead. A general construction for parallelizing Metropolis-Hastings algorithms. Proceedings of the National Academy of Sciences, 111(49):17408–17413, 2014.
 
+## Installation
+
+To run the project install its requirements as follows.
+
+```bash
+pip3 install scipy
+pip3 install numpy
+pip3 install matplotlib
+
+```
+
 # Getting started
 
 To use this package, import the file GMH in your python script
